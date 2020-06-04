2e2fda965cb9d90a3d3435b0a35f126d56d9558a
Sarah Zhong <szhong@redhat.com>
2020-06-04 16:26:49 -0400

    setting up database container

---
+++ backend/db_container_setup.md
@@ -0,0 +1,20 @@
+# Setting up MariaDB container
+
+### Process to set up MariaDB container using image from registry.redhat.io
+(Used docker for building the container but same commands apply for podman)
+
+```
+docker login registry.redhat.io <username>
+docker run -p 3307:3306 --name mymaria1 -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_DATABASE=items -e MYSQL_ROOT_PASSWORD=mypass -d registry.access.redhat.com/rhscl/mariadb-102-rhel7
+```
+
+### Running database inside the container
+```
+docker exec -it mymaria /bin/bash
+mysql -uroot
+```
+
+### Accessing database from outside the container with MySQL
+```
+mysql -h 0.0.0.0 -P 3307 --protocol=TCP -u root -p
+```
--- frontend/rh_cert_notify.py
+++ frontend/rh_cert_notify.py
@@ -9,6 +9,8 @@ import numpy as np
 import re
 import smtplib
 import ssl
+import mysql.connector as mariadb
+import datetime
 
 def get_info(lists_len):
     data_table = []
@@ -16,11 +18,13 @@ def get_info(lists_len):
     while x < lists_len:
         list_data = dfs[1].iloc[x][0]
         split_data = re.split('Date:|Current|Until:|Technologies|Used:', str(list_data))
-        exam_name = split_data[0]
-        exam_date = split_data[1]
-        exam_ex_date = split_data[3]
-        tech_used = split_data[5]
-        data_table.append([exam_name,tech_used,exam_date,exam_ex_date])
+        exam_name = split_data[0].strip()
+        exam_date = split_data[1].strip()
+        exam_date_obj = datetime.datetime.strptime(exam_date, '%b %d, %Y')
+        exam_ex_date = split_data[3].strip()
+        exam_ex_date_obj = datetime.datetime.strptime(exam_date, '%b %d, %Y')
+        tech_used = split_data[5].strip()
+        data_table.append({"cert": exam_name,"tech": tech_used, "date_received": exam_date_obj.date(), "date_expire": exam_ex_date_obj.date()})
         x += 3
     return data_table 
     
@@ -31,7 +35,52 @@ src_url = 'https://www.redhat.com/rhtapps/services/verify?certId=140-177-544'
 
 dfs = pd.read_html(src_url)
 owner_info = dfs[0].iloc[1][0] + ' ' + dfs[0].iloc[1][1]
-print(owner_info)
-list_len = len(dfs[1]) - 3
+owner_name = dfs[0].iloc[1][1]
+cert_id = dfs[0].iloc[0][0].split()[0]
+print (owner_name + ' ' + cert_id)
+list_len = len(dfs[1])
 result = get_info(list_len)
 print(result)
+
+cert_alias = {
+    'Red Hat Certified Specialist in Ansible Best Practices':"ansible_best_prac", 
+    'Red Hat Certified Specialist in Ansible Automation':"ansible_auto",
+    'Red Hat Certified Engineer':"RHCE",
+    'Red Hat Certified System Administrator':"RHCSA"
+    }
+
+try:
+   conn = mariadb.connect(
+      user="root",
+      password="mypass",
+      host="0.0.0.0",
+      port=3308,
+      database='certs')
+   print ("DB connection success")
+
+   cursor = conn.cursor()
+
+   # Creating tables
+   cursor.execute("create table RHCE (name varchar(255), email varchar(255), cert_id varchar(255), tech varchar(255), date_received date, date_expire date, PRIMARY KEY(name))")
+   cursor.execute("create table RHCSA (name varchar(255), email varchar(255), cert_id varchar(255), tech varchar(255), date_received date, date_expire date, PRIMARY KEY(name))")
+   cursor.execute("create table ansible_auto (name varchar(255), email varchar(255), cert_id varchar(255), tech varchar(255), date_received date, date_expire date, PRIMARY KEY(name))")
+   cursor.execute("create table ansible_best_prac (name varchar(255), email varchar(255), cert_id varchar(255), tech varchar(255), date_received date, date_expire date, PRIMARY KEY(name))")
+
+   # Insert data into table
+   for curr_cert in result:
+       table_name = cert_alias.get(curr_cert['cert'])
+       sql = "insert into {exam} (name, cert_id, tech, date_received, date_expire) values (%s,%s,%s,%s,%s)".format(exam=table_name)
+       data = (owner_name, cert_id, curr_cert['tech'], curr_cert['date_received'], curr_cert['date_expire'])
+       cursor.execute(sql,data)
+       conn.commit()
+       print ("Insert success into " + table_name)
+
+
+except mariadb.Error as e:
+   print(f"Error connecting to MariaDB Platform: {e}")
+
+finally:
+   if (conn.is_connected()):
+      cursor.close()
+      conn.close()
+      print("DB connection closed")

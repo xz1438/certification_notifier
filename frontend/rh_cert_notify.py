d753b746e064ea55ea5a5ec630f3beeee631ed9e
Abner Malivert <amaliver@redhat.com>
2020-06-16 15:52:32 -0400

    added ldap search function

--- frontend/rh_cert_notify.py
+++ frontend/rh_cert_notify.py
@@ -9,6 +9,7 @@ import numpy as np
 import smtplib
 import re
 import ssl
+import ldap
 
 from email.message import EmailMessage
 
@@ -18,7 +19,34 @@ recipient is one email
 Password for redhat account will be standard input for the time being
 Execute with email_recipient("example@company.com")
 """
-def email_recipient(recipient):
+def get_info(lists_len,dfs):
+    data_table = []
+    x = 0
+    while x < lists_len:
+        list_data = dfs[1].iloc[x][0]
+        split_data = re.split('Date:|Current|Until:|Technologies|Used:', str(list_data))
+        exam_name = split_data[0]
+        exam_date = split_data[1]
+        exam_ex_date = split_data[3]
+        tech_used = split_data[5]
+        data_table.append([exam_name,tech_used,exam_date,exam_ex_date])
+        x += 3
+    return data_table 
+
+def get_owner_name(dfs):
+    owner_info = dfs[0].iloc[1][0] + ' ' + dfs[0].iloc[1][1]
+    return owner_info
+
+
+def get_owner_info(owner):
+    ldap_server = ldap.initialize('ldap://ldap.corp.redhat.com')
+    ldap_server.simple_bind('dc=redhat,dc=com')
+    search_result = ldap_server.search("dc=redhat,dc=com", ldap.SCOPE_SUBTREE, "cn=%s" % owner)
+    ldap_server.set_option(ldap.OPT_REFERRALS, 0)
+    result_type, result_data = ldap_server.result(search_result, 0)
+    
+
+def send_email(recipient):
     sender = "rhemailnotifier@gmail.com"
     msg = EmailMessage()
 
@@ -34,28 +62,16 @@ def email_recipient(recipient):
     s.sendmail(sender, recipient, msg.as_string())
     s.quit()
 
-def get_info(lists_len):
-    data_table = []
-    x = 0
-    while x < lists_len:
-        list_data = dfs[1].iloc[x][0]
-        split_data = re.split('Date:|Current|Until:|Technologies|Used:', str(list_data))
-        exam_name = split_data[0]
-        exam_date = split_data[1]
-        exam_ex_date = split_data[3]
-        tech_used = split_data[5]
-        data_table.append([exam_name,tech_used,exam_date,exam_ex_date])
-        x += 3
-    return data_table 
+def  main():
+    # specify the url
+    src_url = 'https://www.redhat.com/rhtapps/services/verify?certId=140-177-544'
+    dfs = pd.read_html(src_url)
+    owner = get_owner_name(dfs)
+    # owner_info = dfs[0].iloc[1][0] + ' ' + dfs[0].iloc[1][1]
+    # print(owner_info)
+    list_len = len(dfs[1]) - 3
+    result = get_info(list_len,dfs)
+    #print(result)
 
-# specify the url
-src_url = 'https://www.redhat.com/rhtapps/services/verify?certId=140-177-544'
-
-# parse the html using beautiful soup and store in variable `soup`
-
-dfs = pd.read_html(src_url)
-owner_info = dfs[0].iloc[1][0] + ' ' + dfs[0].iloc[1][1]
-print(owner_info)
-list_len = len(dfs[1]) - 3
-result = get_info(list_len)
-print(result)
+if __name__ == "__main__":
+    main()
